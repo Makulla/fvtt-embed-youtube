a7d0c73da64c73bf5ce8f775d0c2e3bd5882d984
Makulla <matthias.makulla@gmx.de>
2022-02-11 17:22:48 +0100

    compile

--- dist/scripts/player.js
+++ dist/scripts/player.js
@@ -1,33 +1,30 @@
 "use strict";
-let dialog = undefined;
-if (window.fvttYtHookId) {
-    Hooks.off('createChatMessage', window.fvttYtHookId);
-}
-window.fvttYtHookId = Hooks.on("createChatMessage", (msg) => {
-    let split = msg.data.content.split(':');
-    if (split[0] !== 'YTVID') {
-        return;
-    }
-    dialog = dialog !== null && dialog !== void 0 ? dialog : new Dialog({
+let dialogHolder = {};
+const playVideo = (videoId) => {
+    var _a;
+    dialogHolder.dialog = (_a = dialogHolder.dialog) !== null && _a !== void 0 ? _a : new Dialog({
         title: 'Embed Youtube',
         content: '',
         default: '',
         buttons: {},
-        close: () => window.fvttYtDialog = undefined
+        close: () => dialogHolder.dialog = undefined
     }, {
         width: 470,
         height: 300,
         resizable: true,
         minimizable: true
     });
-    const ytVidId = split[1];
     const content = '<iframe \
         width="450" \
         height="250" \
         allow="autoplay" \
-        src="https://www.youtube.com/embed/' + ytVidId + '?&autoplay=1&loop=1&playlist=' + ytVidId + '" \
+        src="https://www.youtube.com/embed/' + videoId + '?&autoplay=1&loop=1&playlist=' + videoId + '" \
         frameborder="0" \
     ';
-    window.fvttYtDialog.data.content = content;
-    window.fvttYtDialog.render(true);
+    dialogHolder.dialog.data.content = content;
+    dialogHolder.dialog.render(true);
+};
+Hooks.once("socketlib.ready", () => {
+    const socket = socketlib.registerModule("embed-youtube");
+    socket.register("play-video", id => playVideo(id));
 });
