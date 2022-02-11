baa54f1eedbb01ae115a22050db7177a89a1761c
Makulla <matthias.makulla@gmx.de>
2022-02-11 15:21:18 +0100

    add package.json and typescript, move script.js to script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,26 +1,35 @@
-"use strict";
-if (window.fvttYtHookId) {
-    Hooks.off('createChatMessage', window.fvttYtHookId);
+interface Window {
+    fvttYtHookId?: number;
+    fvttYtDialog?: Dialog;
 }
-window.fvttYtHookId = Hooks.on("createChatMessage", (msg) => {
-    var _a;
+
+
+if(window.fvttYtHookId) {
+    Hooks.off('createChatMessage', window.fvttYtHookId);
+ }
+ 
+ window.fvttYtHookId = Hooks.on("createChatMessage", (msg: ChatMessage) => {
+ 
     let split = msg.data.content.split(':');
-    if (split[0] !== 'YTVID') {
-        return;
-    }
-    window.fvttYtDialog = (_a = window.fvttYtDialog) !== null && _a !== void 0 ? _a : new Dialog({
-        title: 'Embed Youtube',
+    
+    if(split[0] !== 'YTVID') { return; }
+    
+    window.fvttYtDialog = window.fvttYtDialog ?? new Dialog({ 
+        title: 'Embed Youtube', 
         content: '',
         default: '',
-        buttons: {},
+        buttons: {}, 
         close: () => window.fvttYtDialog = undefined
-    }, {
+    },
+    { 
         width: 470,
         height: 300,
         resizable: true,
         minimizable: true
     });
+    
     const ytVidId = split[1];
+    
     const content = '<iframe \
         width="450" \
         height="250" \
@@ -28,6 +37,8 @@ window.fvttYtHookId = Hooks.on("createChatMessage", (msg) => {
         src="https://www.youtube.com/embed/' + ytVidId + '?&autoplay=1&loop=1&playlist=' + ytVidId + '" \
         frameborder="0" \
     ';
+    
     window.fvttYtDialog.data.content = content;
+    
     window.fvttYtDialog.render(true);
-});
+ });
